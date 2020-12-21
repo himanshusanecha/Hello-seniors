1333bc8e6d476fea0365ac26584ce6d53ec5cacb
Himanshu Sanecha <52252631+himanshusanecha@users.noreply.github.com>
2020-12-21 14:53:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var btn = document.getElementById("button");
+btn.addEventListener("click",function(){
+    var text = document.getElementById("text").value;
+    if(text=="akshita")
+    {
+        var card1 = document.getElementById("card1");
+        card1.style.display = "block";
+    }
+    if(text=="keshav")
+    {
+        var card2 = document.getElementById("card2");
+        card2.style.display = "block";
+    }
+    if(text=="preeti")
+    {
+        var card3 = document.getElementById("card3");
+        card3.style.display = "block";
+    }
+    if(text=="abhimanyu")
+    {
+        var card4 = document.getElementById("card4");
+        card4.style.display = "block";
+    }
+    if(text=="anjali")
+    {
+        var card5 = document.getElementById("card5");
+        card5.style.display = "block";
+    }
+    
+   // if(text=="")
+});
